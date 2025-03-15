2aacf4fe11f2c247c90b0f04600e23d3cf50537e
Sebastian Moreno <sebastian123177@gmail.com>
2025-03-15 11:40:34 -0500

    Autor

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+/* Desarrollado por: Sebastian Moreno */
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. 
 // Aquí deberás desarrollar la lógica para resolver el problema.
 let amigos = [];
